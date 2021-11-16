5e0cfa5caf7d013c5327d96e0c76d3f2fd461f9e
Tiago Rodrigues <tiagopbr.2001@gmail.com>
2021-11-16 19:32:29 +0000

    Changes output so that it adds the minus sign to negative numbers

--- First-Project/BigNumber.hs
+++ First-Project/BigNumber.hs
@@ -1,6 +1,16 @@
 type Digit = Integer
 
-type BigNumber = [Integer]
+{- Bool was added to check for the sign of the number -}
+type BigNumber = (Bool, [Digit])
 
 output :: BigNumber -> String
-output = concatMap show
+output num
+  | fst num = concatMap show (snd num)
+  | otherwise = reverse (reverse (concatMap show (snd num)) ++ "-")
+
+digs :: Integral x => x -> [x]
+digs 0 = []
+digs x = digs (x `div` 10) ++ [x `mod` 10]
+
+--scanner :: String -> BigNumber
+--scanner str = digs (read (snd str))
