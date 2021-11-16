9cb0b97524087536d46db764edb77c2626242d90
Tiago Rodrigues <tiagopbr.2001@gmail.com>
2021-11-16 20:20:56 +0000

    Adds second way to add BigNumbers

--- First-Project/BigNumber.hs
+++ First-Project/BigNumber.hs
@@ -20,6 +20,17 @@ scanner str
 somaBN :: BigNumber -> BigNumber -> BigNumber
 somaBN n1 n2 = scanner (show (read (output n1) + read (output n2)))
 
+somaBN2 :: BigNumber -> BigNumber -> BigNumber
+somaBN2 n1 n2
+  | fst n1 && fst n2 = scanner (show (num1 + num2))
+  | fst n1 && not (fst n2) = scanner (show (num1 - num2))
+  | not (fst n1) && fst n2 = scanner (show (- num1 + num2))
+  | not (fst n1) && not (fst n2) = scanner (show (- num1 - num2))
+  | otherwise = error "Invalid BigNumber's"
+  where
+    num1 = sum [(10 ^ n) * (reverse (snd n1) !! n) | n <- [0 .. length (snd n1) - 1]]
+    num2 = sum [(10 ^ n) * (reverse (snd n2) !! n) | n <- [0 .. length (snd n2) - 1]]
+
 subBN :: BigNumber -> BigNumber -> BigNumber
 subBN n1 n2 = scanner (show (read (output n1) + ((-1) * read (output n2))))
 
