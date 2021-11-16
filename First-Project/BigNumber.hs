ddb5a4293532558d9afd6a24e8ff7599d9090aa8
Tiago Rodrigues <tiagopbr.2001@gmail.com>
2021-11-16 19:46:24 +0000

    Fixes scanner to work with new type of BigNumber

--- First-Project/BigNumber.hs
+++ First-Project/BigNumber.hs
@@ -6,11 +6,13 @@ type BigNumber = (Bool, [Digit])
 output :: BigNumber -> String
 output num
   | fst num = concatMap show (snd num)
-  | otherwise = reverse (reverse (concatMap show (snd num)) ++ "-")
+  | otherwise = '-' : concatMap show (snd num)
 
 digs :: Integral x => x -> [x]
 digs 0 = []
 digs x = digs (x `div` 10) ++ [x `mod` 10]
 
---scanner :: String -> BigNumber
---scanner str = digs (read (snd str))
+scanner :: String -> BigNumber
+scanner str
+  | head str == '-' = (False, digs (read (tail str)))
+  | otherwise = (True, digs (read str))
