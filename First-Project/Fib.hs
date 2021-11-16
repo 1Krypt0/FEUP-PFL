c222792689cc6de3ceac56714f91649d5adb572d
Tiago Rodrigues <tiagopbr.2001@gmail.com>
2021-11-16 14:42:42 +0000

    Adds Fib.hs to repo

---
+++ First-Project/Fib.hs
@@ -0,0 +1,18 @@
+{- Recursive version of the Fibonnaci sequence-}
+fibRec :: (Integral a) => a -> a
+fibRec n
+  | n == 0 = 0
+  | n == 1 = 1
+  | n > 1 = fibRec (n - 1) + fibRec (n - 2)
+  | otherwise = error "Invalid argument"
+
+{-Find a way to store the used values in the fibs function without being an infinite list-}
+fibLista :: (Integral a) => a -> a
+fibLista n = fibs !! fromIntegral n
+  where
+    fibs = 0 : 1 : zipWith (+) fibs (drop 1 fibs)
+
+fibListaInfinita :: (Integral a) => a -> a
+fibListaInfinita n = fibs !! fromIntegral n
+  where
+    fibs = 0 : 1 : zipWith (+) fibs (tail fibs)
