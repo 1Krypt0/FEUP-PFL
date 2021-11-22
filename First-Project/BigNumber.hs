d08a9dde5e37f05bb2c247eb4f3dc23585e1ebef
Tiago Rodrigues <tiagopbr.2001@gmail.com>
2021-11-22 14:58:01 +0000

    Makes addition work with equal signs on both

--- First-Project/BigNumber.hs
+++ First-Project/BigNumber.hs
@@ -15,36 +15,44 @@ scanner str
   | head str == '-' = (False, negl)
   | otherwise = (True, l)
   where
-    l = map (toInteger . digitToInt) str
-    negl = map (toInteger . digitToInt) (tail str)
+    l = reverse (map (toInteger . digitToInt) str)
+    negl = reverse (map (toInteger . digitToInt) (tail str))
+
+xor :: Bool -> Bool -> Bool
+xor a b = (a && not b) || (not a && b)
 
 somaBN :: BigNumber -> BigNumber -> BigNumber
-somaBN n1 n2 = scanner (show (read (output n1) + read (output n2)))
-
-somaBN2 :: BigNumber -> BigNumber -> BigNumber
-somaBN2 n1 n2
-  | fst n1 && fst n2 = scanner (show (num1 + num2))
-  | fst n1 && not (fst n2) = scanner (show (num1 - num2))
-  | not (fst n1) && fst n2 = scanner (show (- num1 + num2))
-  | not (fst n1) && not (fst n2) = scanner (show (- num1 - num2))
+somaBN n1 n2
+  | not (xor (fst n1) (fst n2)) = positiveSum n1 n2
+  | xor (fst n1) (fst n2) = negativeSum n1 n2
   | otherwise = error "Invalid BigNumber"
-  where
-    num1 = sum [(10 ^ n) * (reverse (snd n1) !! n) | n <- [0 .. length (snd n1) - 1]]
-    num2 = sum [(10 ^ n) * (reverse (snd n2) !! n) | n <- [0 .. length (snd n2) - 1]]
+
+negativeSum :: BigNumber -> BigNumber -> BigNumber
+negativeSum n1 n2
+  | fst n1 = subBN n1 n2
+  | fst n2 = subBN n2 n1
+  | otherwise = error "Invalid BigNumber"
 
 subBN :: BigNumber -> BigNumber -> BigNumber
-subBN n1 n2 = scanner (show (read (output n1) + ((-1) * read (output n2))))
-
-subBN2 :: BigNumber -> BigNumber -> BigNumber
-subBN2 n1 n2
-  | fst n1 && fst n2 = scanner (show (num1 - num2))
-  | fst n1 && not (fst n2) = scanner (show (num1 + num2))
-  | not (fst n1) && fst n2 = scanner (show (- num1 - num2))
-  | not (fst n1) && not (fst n2) = scanner (show (- num1 + num2))
-  | otherwise = error "Invalid BigNumber"
+subBN n1 n2
+  | fst n1 && not (fst n2) = somaBN n1 num2
+  | otherwise = error "Fodeu"
   where
-    num1 = sum [(10 ^ n) * (reverse (snd n1) !! n) | n <- [0 .. length (snd n1) - 1]]
-    num2 = sum [(10 ^ n) * (reverse (snd n2) !! n) | n <- [0 .. length (snd n2) - 1]]
+    num2 = (True, snd n2)
 
-mulBN :: BigNumber -> BigNumber -> BigNumber
-mulBN n1 n2 = scanner (show (read (output n1) * read (output n2)))
+dropTrailingZeroes :: [Digit] -> [Digit]
+dropTrailingZeroes l = reverse (dropWhile (== 0) (reverse l))
+
+stuffZeroes :: [Digit] -> [Digit] -> [Digit]
+stuffZeroes l remainder = replicate (length remainder - length l) 0
+
+positiveSum :: BigNumber -> BigNumber -> BigNumber
+positiveSum n1 n2
+  | fst n1 && fst n2 = (True, dropTrailingZeroes res)
+  | not (fst n1) && not (fst n2) = (False, dropTrailingZeroes res)
+  | otherwise = error "Invalid input, somehow"
+  where
+    res = [mod (x + y + z) 10 | (x, y, z) <- zip3 num1 num2 remainder]
+    remainder = 0 : [if n >= 10 then 1 else 0 | n <- zipWith (+) (snd n1) (snd n2)]
+    num1 = snd n1 ++ stuffZeroes (snd n1) remainder
+    num2 = snd n2 ++ stuffZeroes (snd n2) remainder
