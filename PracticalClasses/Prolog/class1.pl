798735ec3c5d77acbe3732449fd17906b7430ae7
Tiago Rodrigues <tiagopbr.2001@gmail.com>
2021-12-12 19:19:38 +0000

    Completes exercise 1 from first class

--- PracticalClasses/Prolog/class1.pl
+++ PracticalClasses/Prolog/class1.pl
@@ -3,7 +3,7 @@
 %
 
 % a) Given the family tree, represent it in Prolog using the male,
-% female and parent predicates.
+%    female and parent predicates.
 
 % Females
 female(grace).
@@ -96,7 +96,7 @@ parent(haley, george).
 parent(dylan, poppy).
 parent(haley, poppy).
 
-% b) Using the interpreter, obtain the answers to the following questions
+% b) Using the interpreter, obtain the answers to the following questions:
 %
 % i. Is haley a female? female(haley) -> yes
 % ii. Is Gil a male? male(Gil) -> no
@@ -110,8 +110,71 @@ parent(haley, poppy).
 
 % c) Write rules that define more complex familiar relations, like father,
 %    grandparent, grandmother, siblings, halfSiblings, cousins and uncle.
-
 father(P, Child) :- parent(P, Child), male(P).
-grandparent(GP, Grandkid) :- parent(GP, X), parent(X, Grandkid), male(GP).
-grandmother(GM, Grandkid) :- parent(GM, X), parent(X, Grandkid), male(GM).
-siblings(S1, S2) :- parent(X, S1), parent(X, S2)
+grandparent(GP, GK) :- parent(GP, P), parent(P, GK), male(GP).
+grandmother(GM, GK) :- parent(GM, P), parent(P, GK), female(GM).
+siblings(S1, S2) :- parent(P1, S1), parent(P1, S2), parent(P2, S1), parent(P2, S2), P1 @< P2, S1 \= S2.
+halfSiblings(S1, S2) :- parent(P, S1), parent(P, S2), S1 \= S2, \+siblings(S1, S2).
+cousins(A, B) :- parent(P1, A), parent(P2, B), siblings(P1, P2).
+uncle(U, N) :- parent(P, N), siblings(U, P), male(U).
+aunt(A, N) :- parent(P, N), siblings(P, A), female(A).
+
+% e) What predicates would you use to represent information regarding divorce and marriage?
+%
+% married(Husband, Wife, Year).
+%
+% divorced(Husband, Wife, Year).
+
+
+%
+% Question 2
+%
+
+% a) Represent information relative to the curricular units, professors and students, according to the given
+%    table, using the predicates leciona/2, frequenta/2, where the first argument is the curricular unit.
+
+% Teachers
+leciona(algoritmos, adalberto).
+leciona(bases_de_dados, bernardete).
+leciona(compiladores, capitolino).
+leciona(estatistica, diogenes).
+leciona(redes, ermelinda).
+
+% Students of Algorithms
+frequenta(algoritmos, alberto).
+frequenta(algoritmos, bruna).
+frequenta(algoritmos, cristina).
+frequenta(algoritmos, diogo).
+frequenta(algoritmos, eduarda).
+
+% Students of Databases
+frequenta(bases_de_dados, antonio).
+frequenta(bases_de_dados, bruno).
+frequenta(bases_de_dados, cristina).
+frequenta(bases_de_dados, duarte).
+frequenta(bases_de_dados, eduardo).
+
+% Students of compilers
+frequenta(compiladores, alberto).
+frequenta(compiladores, bernardo).
+frequenta(compiladores, clara).
+frequenta(compiladores, diana).
+frequenta(compiladores, eurico).
+
+% Students of statistics
+frequenta(estatistica, antonio).
+frequenta(estatistica, bruna).
+frequenta(estatistica, claudio).
+frequenta(estatistica, duarte).
+frequenta(estatistica, eva).
+
+% Students of Networks
+frequenta(redes, alvaro).
+frequenta(redes, beatriz).
+frequenta(redes, claudio).
+frequenta(redes, diana).
+frequenta(redes, eduardo).
+
+% b) Use the interpreter to answer the following questions:
+%
+% i. Which UC does Diogenes teach? leciona(UC, diogenes) -> estatistica
