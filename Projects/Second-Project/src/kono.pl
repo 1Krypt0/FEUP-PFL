ed6c17bca98dab036aeac567bbca10f248c36ffb
Tiago Rodrigues <tiagopbr.2001@gmail.com>
2021-12-27 23:14:15 +0000

    Add game loop for PvP version

--- Projects/Second-Project/src/kono.pl
+++ Projects/Second-Project/src/kono.pl
@@ -12,9 +12,28 @@ play :-
 * Initial board configuration
 */
 initial([
-    [2, 2, 2, 2, 2],
-    [2, 0, 0, 0, 2],
+    [1, 1, 1, 1, 1],
+    [1, 0, 0, 0, 1],
     [0, 0, 0, 0, 0],
     [1, 0, 0, 0, 1],
     [1, 1, 1, 1, 1]
 ]).
+
+playPvP :-
+    initial(Board),
+    display_game(Board, 1),
+    playPvP(Board, 1, _).
+
+playPvP(Board, Player, Result) :-
+    read_move(Board, Player, Move),
+    move(Board, Player, Move, NewBoard), !,
+    next_player(Player, NextPlayer),
+    display_game(NewBoard, NextPlayer), !,
+    (
+        game_over(NewBoard, Result),
+        dif(Result, -1),
+        announce(Result, Player),
+        pause
+        ;
+        playPvP(NewBoard, NextPlayer, Result)
+    ).
