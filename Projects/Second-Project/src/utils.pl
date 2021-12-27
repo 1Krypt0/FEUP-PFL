9f4a3e68fcebd23db2bfe32cf35215bb2c3303ed
Tiago Rodrigues <tiagopbr.2001@gmail.com>
2021-12-27 19:09:17 +0000

    Start moving some functions to utils

---
+++ Projects/Second-Project/src/utils.pl
@@ -0,0 +1,20 @@
+/**
+ * Int to Char map
+ */
+letter(0, 'A').
+letter(1, 'B').
+letter(2, 'C').
+letter(3, 'D').
+letter(4, 'E').
+letter(5, 'F').
+letter(6, 'G').
+letter(7, 'H').
+letter(8, 'I').
+letter(9, 'J').
+letter(10, 'K').
+letter(11, 'L').
+letter(12, 'M').
+letter(13, 'N').
+letter(14, 'O').
+letter(15, 'P').
+letter(16, 'Q').
