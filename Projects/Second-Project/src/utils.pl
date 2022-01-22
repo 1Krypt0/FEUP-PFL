3086ca9327f3b1ff0d67f3b9fccc908b4522adc7
Tiago Rodrigues <tiagopbr.2001@gmail.com>
2022-01-22 17:52:57 +0000

    Finish utils documentation

--- Projects/Second-Project/src/utils.pl
+++ Projects/Second-Project/src/utils.pl
@@ -1,6 +1,8 @@
-/**
- * Int to Char map
- */
+/*
+* letter(?Number, ?Letter)
+*
+* Maps integers to chars
+*/
 letter(0, 'A').
 letter(1, 'B').
 letter(2, 'C').
@@ -18,12 +20,36 @@ letter(13, 'N').
 letter(14, 'O').
 letter(15, 'P').
 letter(16, 'Q').
+letter(17, 'R').
+letter(18, 'S').
+letter(19, 'T').
+letter(20, 'U').
+letter(21, 'V').
+letter(22, 'W').
+letter(23, 'X').
+letter(24, 'Y').
+letter(25, 'Z').
 
+/*
+* mymember(+Value, +List)
+*
+* Determines if Value belongs to List
+*/
 mymember(X,[X|_]).
 mymember(X,[_|T]) :- mymember(X,T).
 
+/*
+* not(Value)
+*
+* Negates the output of A
+*/
 not(A) :- \+ call(A).
 
+/*
+* set(+List, -Set)
+*
+* Converts a List into a Set by removing duplicates
+*/
 set([],[]).
 set([H|T],[H|Out]) :-
     not(mymember(H,T)),
